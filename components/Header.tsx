b5f03b3c96e08f4bd7b42fe20845419208d5347e
VincentCarlosTrono <trono.vincentcii@gmail.com>
2022-07-02 12:46:52 +0800

    added new details

--- components/Header.tsx
+++ components/Header.tsx
@@ -81,7 +81,7 @@ export default function Header() {
               </nav>
             </Link>
           ))}
-          <Link href="mailto:ed.tosoy@gmail.com">
+          <Link href="">
             <button
               className={`py-2 sm:py-3 px-6  border-2 rounded-full secondary-btn ${
                 scroll && "primary-btn border-none shadow-none"
