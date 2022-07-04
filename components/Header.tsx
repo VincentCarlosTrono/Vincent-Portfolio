3f1414422676e252227310b43550b9936613451f
VincentCarlosTrono <trono.vincentcii@gmail.com>
2022-07-04 15:44:16 +0800

    added downloadable cv

--- components/Header.tsx
+++ components/Header.tsx
@@ -81,7 +81,7 @@ export default function Header() {
               </nav>
             </Link>
           ))}
-          <Link href="">
+          <Link href="https://github.com/VincentCarlosTrono/ResumeBg/raw/main/Vincent-Trono-Resme.pdf">
             <button
               className={`py-2 sm:py-3 px-6  border-2 rounded-full secondary-btn ${
                 scroll && "primary-btn border-none shadow-none"
